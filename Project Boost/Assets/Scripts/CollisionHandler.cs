592ecd09b89c10da97cab2e8e4fbddffa2016060
Wicconite <ethangill1999@outlook.com>
2022-07-05 20:49:40 +0100

    Collision/Scene changing
    Added multiple scenes/levels.
    
    Made a loop so once all levels are complete then it loops back to the first level.. Made sure if you crash then you go back to the start or if you complete the level you proceed to the next one.

--- Project Boost/Assets/Scripts/CollisionHandler.cs
+++ Project Boost/Assets/Scripts/CollisionHandler.cs
@@ -1,16 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    void Start()
+
+    [SerializeField] float timeDelay = 1f;
+
+    void OnCollisionEnter(Collision other) 
     {
-        
+         switch (other.gameObject.tag)
+         {
+            case "Friendly":
+                Debug.Log("Hey, I'm Friendly!");
+                break;
+            case "Finish":
+                FinishSequence();
+                break;
+            default:
+                StartCrashSequence();
+                break;
+         }
     }
 
-    void Update()
+    void StartCrashSequence()
     {
-        
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", timeDelay);
+    }
+
+    void FinishSequence()
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("NextLevel", timeDelay);
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void NextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
     }
 }
